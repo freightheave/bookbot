ed1cf81b5f3c76baf6bf1779c288fd2ddea77ecd
freightheave <komaravolu.roshan@gmail.com>
2024-08-12 07:29:47 +0530

    Minor refactoring and filename change

--- bookbot.py
+++ bookbot.py
@@ -5,10 +5,10 @@ banlist_chars = string.punctuation + string.whitespace
 def main():
     #initialising
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    text = open("books/frankenstein.txt").read()
 
     #create set of unique lowercase chars
-    lower_case_text = convert_lower(text)
+    lower_case_text = text.lower()
     set_of_characters = set(lower_case_text)
     set_of_characters = list(sorted(set_of_characters))
 
@@ -18,12 +18,12 @@ def main():
         if character not in banlist_chars:
             dict_of_chars[character] = 0
 
-    #counting chars
+    #calculations
     dict_of_chars = count_chars(lower_case_text, dict_of_chars)
-
     char_list = create_list(dict_of_chars)
     char_list.sort(reverse=True, key=sort_on)
 
+    #printing report
     print("---------- BEGIN REPORT ----------")
     print("\n")
     print(f"{count_words(text)} words are in Frankenstein.")
@@ -33,25 +33,18 @@ def main():
     print("\n")
     print("---------- END REPORT ----------")
 
+def sort_on(dictionary):
+    return dictionary['num']
+
 def create_list(dictionary):
     char_list = []
     for elements in dictionary:
         char_list.append({"char":elements, "num":dictionary[elements]})
     return char_list
 
-def sort_on(dictionary):
-    return dictionary['num']
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
 def count_words(text):
     words_count = text.split()
     return len(words_count)
-
-def convert_lower(text):
-    return text.lower()
 
 def count_chars(text, dictionary):
     for character in text:
@@ -61,3 +54,8 @@ def count_chars(text, dictionary):
 
 main()
 
+#def get_book_text(path):
+#    with open(path) as f:
+#        return f.read()
+#def convert_lower(text):
+#    return text.lower()
